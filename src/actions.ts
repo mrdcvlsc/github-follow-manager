00da47292f1f9b2d24eee9644fc4f1762f4ca043
mrdcvlsc <mrdcvlsc@gmail.com>
2022-09-17 12:47:11 +0800

    added block and unblock actions

--- src/actions.ts
+++ src/actions.ts
@@ -12,11 +12,21 @@ export type follow_response_t =
 export type unfollow_response_t =
   Endpoints["DELETE /user/following/{username}"]["response"];
 
+export type block_response_t =
+  Endpoints["PUT /user/blocks/{username}"]["response"];
+
+export type unblock_response_t =
+  Endpoints["DELETE /user/blocks/{username}"]["response"];
+
+export type actions_method_response_t = 
+  Promise<follow_response_t> | Promise<unfollow_response_t> |
+  Promise<block_response_t> | Promise<unblock_response_t>;
+
 export type actions_method_t = (
   octokit: Octokit,
   user: string,
   options?: actions_option_t
-) => Promise<follow_response_t> | Promise<unfollow_response_t>;
+) => actions_method_response_t;
 
 export async function asyncSetTimeout(milliseconds: number) {
   return new Promise((resolve) => {
@@ -79,6 +89,60 @@ export class Actions {
     );
     console.log("    -> unfollowed:", user);
     await asyncSetTimeout((options.sleep ?? 1) * 1000);
+    return response;
+  }
+
+  /** block a given user - ***BEWARE! BLOCK REQUEST ARE MORE SUBJECTED TO AGRESSIVE SECONDARY RATE LIMITING!***.
+   *
+   * @param octokit `Octokit` instance.
+   *
+   * @param user github account to be blocked.
+   *
+   * @param options.sleep **optional** delay in **seconds** after performing the request (**default `2`**).
+   *
+   * @param options.retryAfter **optional** delay in **seconds** after hitting the
+   * **secondary rate limit** before performing another request (**default `60`**).
+   */
+   public static async block(
+    octokit: Octokit,
+    user: string,
+    options: actions_option_t = {}
+  ): Promise<block_response_t> {
+    const response = await octokit.request("PUT /user/blocks/{username}", {
+      username: user,
+      headers: {
+        "Retry-After": options.retryAfter ?? 60,
+      },
+    });
+    console.log("    -> blocked:", user);
+    await asyncSetTimeout((options.sleep ?? 2) * 1000);
+    return response;
+  }
+
+  /** unblock a given user.
+   *
+   * @param octokit `Octokit` instance.
+   *
+   * @param user github account to be unblocked.
+   *
+   * @param options.sleep **optional** delay in **seconds** after performing the request (**default `2`**).
+   *
+   * @param options.retryAfter **optional** delay in **seconds** after hitting the
+   * **secondary rate limit** before performing another request (**default `60`**).
+   */
+   public static async unblock(
+    octokit: Octokit,
+    user: string,
+    options: actions_option_t = {}
+  ): Promise<unblock_response_t> {
+    const response = await octokit.request("DELETE /user/blocks/{username}", {
+      username: user,
+      headers: {
+        "Retry-After": options.retryAfter ?? 60,
+      },
+    });
+    console.log("    -> unblocked:", user);
+    await asyncSetTimeout((options.sleep ?? 2) * 1000);
     return response;
   }
 }
